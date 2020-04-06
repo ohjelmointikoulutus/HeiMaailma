145bb3275ba2fc0c4b7116a78b9ca6e5c8c0086a
Kai Kaltiola <kai.kaltiola@skillege.com>
2020-04-06 09:47:26 +0300

    Luotiin heimaailma-sovellus

---
+++ src/hei.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitesimerkki;
+
+/**
+ *
+ * @author kaika
+ */
+public class GitEsimerkki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hei, maailma!");
+        
+        System.out.println("Hei, Kajaani");
+        
+    }
+    
+}
+

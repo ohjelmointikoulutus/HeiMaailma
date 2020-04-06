95d93e121fd01636b856570454eacdd7b197fae0
Kai Kaltiola <kai.kaltiola@skillege.com>
2020-04-06 10:00:55 +0300

    Muutettiin tulostus englanninkieliseksi

--- src/hei.java
+++ src/hei.java
@@ -16,7 +16,7 @@ public class GitEsimerkki {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hei, maailma!");
+        System.out.println("Hello world!");
         
     }
     
